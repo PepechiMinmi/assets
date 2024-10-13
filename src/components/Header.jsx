68db57940feafdbd6d20ecd20e8acf1bd9598d5c
PepechiMinmi <p5420077@stu.chs.nihon-u.ac.jp>
2024-10-13 17:15:34 +0900

    修正2-コンポーネント作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Nav from "./NavigationMenu";//ナビゲーションメニュー
 export default function Header() {
     return(
         <header>
-            <h1 className="h1">Birdwatching</h1>
-            <img className="header img" src="dove.png" alt="a simple dove logo"></img>
+            <h1>Birdwatching</h1>
+            <img src="../../dove.png" alt="a simple dove logo"></img>
             <Nav />
         </header>
     );
